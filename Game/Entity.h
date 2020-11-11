f4039140606a7d1af213026a07ac19b7235e9c7a
shivus123 <44923749+shivus123@users.noreply.github.com>
2020-11-11 09:33:35 +0100

    moving

--- Game/Entity.h
+++ Game/Entity.h
@@ -8,12 +8,12 @@ private:
 
 protected:
     std::shared_ptr<Context> m_context;
-	std::shared_ptr<sf::Vector2f> pos;
+	std::unique_ptr<sf::RectangleShape> pos;
 	unsigned int health;
 
 public:
-	Entity(std::shared_ptr<Context> context, std::shared_ptr<sf::Vector2f> poss, unsigned int health) :m_context(context), pos(poss), health(health) {};
-	virtual ~Entity() { };
+	Entity(std::shared_ptr<Context> context, std::shared_ptr<sf::Vector2f> poss, unsigned int health) :m_context(context), pos(std::make_unique<sf::RectangleShape>(*poss)), health(health) {};
+	virtual ~Entity() {};
 
 	virtual void update(sf::Time deltaTime) = 0;
 	virtual void render() = 0;
--- Game/Player.cpp
+++ Game/Player.cpp
@@ -2,13 +2,13 @@
 
 
 m_Entity::Player::~Player()
-{}
+{
+}
 
 void m_Entity::Player::render()
 {
     //  m_context->m_window->draw()
-    m_context->m_window->draw(sf::RectangleShape(*pos));
-
+    m_context->m_window->draw(*pos);
 }
 
 
@@ -20,5 +20,28 @@ void m_Entity::Player::update(sf::Time deltaTime)
 void m_Entity::Player::update(sf::Time deltaTime, std::shared_ptr<std::queue<unsigned int>> keys)
 {
     this->keys = std::move(keys);
+    while (!this->keys->empty())
+    {
+        int k = this->keys->front();
+        switch (k)
+        {
+        case sf::Keyboard::Right:
+            this->pos->move({ 10, 0 });
+            break;
+        case sf::Keyboard::Left:
+            this->pos->move({ -10, 0 });
+            break;
+        case sf::Keyboard::Up:
+            this->pos->move({ 0, -10 });
+            break;
+        case sf::Keyboard::Down:
+            this->pos->move({ 0, 10 });
+            break;
+        default:
+            break;
+
+        }
+        this->keys->pop();
+    }
 }
 
